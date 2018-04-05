d962800e77eede4e834119f9eb74a9c2e3381fee
wunan <1654974471@qq.com>
2018-04-05 23:13:52 +0800

    frist

---
+++ OCStudyDemo/OCStudyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCStudyDemo
+//
+//  Created by 吴楠 on 2018/4/5.
+//  Copyright © 2018年 wunan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCStudyDemo/OCStudyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCStudyDemo
+//
+//  Created by 吴楠 on 2018/4/5.
+//  Copyright © 2018年 wunan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
